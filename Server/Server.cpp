f851890ee59e7cc547e13a3c209e72ab9ea805f4
Klimentiy Dudarev <101472727+Verius50@users.noreply.github.com>
2024-02-27 16:48:22 +0300

    Добавление комментариев

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -16,57 +16,57 @@
 
 std::vector<Client> clients;
 
-void newClient(SOCKET client)
+void newClient(SOCKET client)	//метод для обработки подключения нового клиента
 {
     if (client == INVALID_SOCKET){
         std::cout << "accepting socket error";
         return;
     }
     Client newClient(client);
-    char* buff = new char[256];
+    char* buff = new char[256];		//для данныз о клиенте
     memset(buff, 0, 256);
     int result = 0;
 
-    if (recv(client, buff, 256, 0) > 0) {
+    if (recv(client, buff, 256, 0) > 0) {	//в случае успешного получения данных
         newClient.setUserName(std::string(strtok(buff, "/")));
         newClient.setIp(std::string(strtok(NULL, "/")));
         newClient.setSystemInfo(std::string(strtok(NULL, "/")));
         bool alreadyExist = 0;
-        for (int i = 0; i < clients.size(); ++i)
-              if (clients[i] == newClient) {
+        for (int i = 0; i < clients.size(); ++i)	//проверяем не подключался ли ранее этот же клиент
+              if (clients[i] == newClient) {		//если да, то возвращаем ему статус online (см. client.h operator==(...))
                   alreadyExist = 1;
                   break;
               }
           if (!alreadyExist)
-              clients.push_back(newClient);
+              clients.push_back(newClient);	//если нет, создаём нового
     }
 }
 
 
-void getScreenShot(Client source)
+void getScreenShot(Client source)	//получает скриншот от указанного клиента
 {
 
-    char* screenInfo = new char[15];
+    char* screenInfo = new char[15];	//получаем размеры изображения
     if (recv(source.getSocket(), screenInfo, 15, 0) <= 0) {
         std::cout << "Getting screenshot info failed!";
         return;
     }
-    char* output;
-    int screenWidth = strtol(strtok(screenInfo, " "), &output, 10),
+    char* output;	//для функции strtol
+    int screenWidth = strtol(strtok(screenInfo, " "), &output, 10),	//делим полученную строку на значения
 		screenHeight = strtol(strtok(NULL, " "), &output, 10);
    
     char* screen = new char[3 * screenWidth * screenHeight];
-    if (recv(source.getSocket(), screen, 3 * screenWidth * screenHeight, 0) > 0) {
+    if (recv(source.getSocket(), screen, 3 * screenWidth * screenHeight, 0) > 0) {	//если учпешно получили само изображение
 
-        sf::Image screenbuffer;
+        sf::Image screenbuffer;		//создаём изображение (см. документацию SFML)
         screenbuffer.create(screenWidth, screenHeight);
         sf::Color pixel;
 
-        for (int i = 0; i < 3 * screenWidth * screenHeight; i += 3){
+        for (int i = 0; i < 3 * screenWidth * screenHeight; i += 3){	//перебираем его пиксели (у каждого 3 компонента)
             pixel = sf::Color(screen[i], screen[i + 1], screen[i + 2]);
             screenbuffer.setPixel((i / 3) % screenWidth, (i / 3) / screenWidth, pixel);
         }
-        screenbuffer.saveToFile("Screen from " + source.getUserName() + ".png");
+        screenbuffer.saveToFile("Screen from " + source.getUserName() + ".png");	//созраняем изображение
     } else
     {
         std::cout << "Cant load screenshot!";
@@ -74,9 +74,9 @@ void getScreenShot(Client source)
 }
 
 
-void userInterface()
+void userInterface()	
 {
-    time_t start = time(0), now = time(0) + 1;
+    time_t start = time(0), now = time(0) + 1;		//обновление пользовательского интерфейса раз в секунду
     int answ = 0;
     do {
         now = time(0) + 1;
@@ -89,13 +89,13 @@ void userInterface()
             for (int i = 0; i < clients.size(); ++i) {
                 std::cout << i << ". ";
                 clients[i].print();
-                if (send(clients[i].getSocket(), "m", 1, 0) <= 0)
-                    clients[i].leaves();
+                if (send(clients[i].getSocket(), "m", 1, 0) <= 0)	//проверяем подключен ли клиент
+                    clients[i].leaves();				//если нет, отключаем его
             }
             std::cout << "\n1) Make a screenshot"
                 << "\n2) Quit\n";
         }
-        if (_kbhit())
+        if (_kbhit())	//при нажатии клавиши
         {
             answ = _getch();
             switch (answ)
@@ -104,8 +104,8 @@ void userInterface()
                 int n = 0;
                 std::cout << "Enter client number: ";
                 std::cin >> n;
-                if (n < clients.size() && send(clients[n].getSocket(), "s", 1, 0) >= 0) {
-                    std::thread screenShot(getScreenShot, clients[n]);
+                if (n < clients.size() && send(clients[n].getSocket(), "s", 1, 0) >= 0) {	//отправляем сигнал для получения скриншота
+                    std::thread screenShot(getScreenShot, clients[n]);				//и в отдельном потоке ожилаем его получения
                     screenShot.detach();
                 }
                 break; }
@@ -123,7 +123,7 @@ int main()
 {
     WSAData wsaData;
     ADDRINFO hints, * addrResult = NULL;
-    SOCKET listenSocket = INVALID_SOCKET, client = INVALID_SOCKET;
+    SOCKET listenSocket = INVALID_SOCKET, client = INVALID_SOCKET;	//сокет для прослушивания порта
     int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (result != 0){
         std::cout << "StartUp failed with error " << result;
@@ -153,19 +153,19 @@ int main()
         WSACleanup();
         return 1;
     }
-    result = listen(listenSocket, SOMAXCONN);
+    result = listen(listenSocket, SOMAXCONN);	
     if (result == SOCKET_ERROR){
         std::cout << "Listening failed with error: " << result;
         closesocket(listenSocket);
         freeaddrinfo(addrResult);
         WSACleanup();
     }
-    std::thread user(userInterface);
+    std::thread user(userInterface);	//инициализируем интерфейс в отдельном потоке
     user.detach();
-    while (1) {
-        SOCKET client = accept(listenSocket, NULL, NULL);
+    while (1) {		//ждём подключений
+        SOCKET client = accept(listenSocket, NULL, NULL);	//при подключении
 
-        std::thread thr(newClient, client);
+        std::thread thr(newClient, client);		//в отдельном потоке его обрабатываем
         thr.detach();
     }
 
